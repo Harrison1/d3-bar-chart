7b7996980f571319a4f1b40f76a86786492e74ff
Harrison <hmcguire13@gmail.com>
2016-09-07 21:14:00 -0700

    edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,3 +122,10 @@ $(document).ready(function() {
         }
     });
 });
+
+var barColors = ['#f00', '#abc', '#123'];
+var i = 0;
+$('.bar-to').each(function() {
+    $(this).css('fill', barColors[i]);
+    i = (i + 1) % barColors.length;
+});
