1f82228e3a1bf9a6a2bba35974bf0125f94fb21d
Harrison <hmcguire13@gmail.com>
2016-09-07 23:01:56 -0700

    edits

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,13 +86,13 @@ $(document).ready(function() {
 
         
         bar.append("rect")
-            .attr("transform", "translate(" + labelWidth + ", 0)")
+            .attr("transform", "translate(" + labelWidth-18 + ", 0)")
             .attr("height", barHeight)
             .attr("width", function(d) {                    
                 return scale(parseFloat(d[1]));
             });
         bar.append("text")
-            .attr("transform", "translate(" + labelWidth + ", 0)")
+            .attr("transform", "translate(" + labelWidth-18 + " , 0)")
             .attr("class", "percent-label")
             .attr("y", barHeight / 2)
             .attr("x", function(d){
